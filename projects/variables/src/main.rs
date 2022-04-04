3788be97c0181a6ef8fb913b6da5f3acd181209b
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2022-04-04 21:58:03 +0100

    learn about constants

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
-   let mut x = 5;
-   println!("The value of x is: {}", x);
-   x = 6;
-   println!("The value of x is: {}", x); 
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    //CONSTANTS
+    
+    //just like variables initially, constants are immutable.
+    //unlike variables, constants are always immutable and can never be made mutuable
+
+    const THREE_HOURS_IN_SECONDS : u32 = 60 * 60 * 3;
+    println!("Three hours in seconds is {} seconds", THREE_HOURS_IN_SECONDS);
 }
