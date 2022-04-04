ebcf4e48548f2e28a639450e2973219d6cd81137
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2022-04-04 22:06:30 +0100

    learn about shadowing in variables

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -5,10 +5,31 @@ fn main() {
     println!("The value of x is: {}", x);
 
     //CONSTANTS
-    
+
     //just like variables initially, constants are immutable.
     //unlike variables, constants are always immutable and can never be made mutuable
 
     const THREE_HOURS_IN_SECONDS : u32 = 60 * 60 * 3;
     println!("Three hours in seconds is {} seconds", THREE_HOURS_IN_SECONDS);
+
+    // SHADOWING
+    // means the most recent value in the scope is what the program sees
+    // when the variable is used.
+    
+
+    let x = 5;
+
+    let x = x + 1;
+    
+    {
+        let x = x + 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x in the outer scope is: {}", x);
+
+    // you can change the type but reuse the same name in shadowing
+
+    let spaces = "   ";
+    let spaces = spaces.len();
 }
