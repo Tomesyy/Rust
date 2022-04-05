36e195558814e13069b7e3710950158914f3de09
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2022-04-05 17:02:51 +0100

    convert fibonacci project to interactive CLI tool.

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,11 +1,30 @@
-fn main() {
-    let n = 94;
+use std::io;
 
-    let res = generate_nth_fibonacci_number(n);
-    println!("The {}th fibonacci number is: {}", n, res);
+fn main() {
+
+    loop {
+        println!("Please input a number: ");
+
+        let mut number = String::new();
+
+        io::stdin()
+            .read_line(&mut number)
+            .expect("failed to input");
+        
+        let number: u8 = match number.trim().parse() {
+            Ok(number) => number,
+            Err(_) => {
+                println!("invalid. try inputting another number\n");
+                continue
+            }
+        };
+
+        let res: u32 = fib_recursive(number);
+        println!("The {}th fibonacci number is {}\n", number, res);
+    }
 }
 
-fn generate_nth_fibonacci_number(n: usize) -> usize {
+fn _fib_iterative(n: usize) -> usize {
     let mut arr = [0; 95];
     arr[1] = 1;
 
@@ -14,4 +33,14 @@ fn generate_nth_fibonacci_number(n: usize) -> usize {
     }
 
     arr[n-1]
+}
+
+fn fib_recursive(n: u8) -> u32 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+    return fib_recursive(n-1) + fib_recursive(n-2)
 }
