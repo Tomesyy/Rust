0fd1a7b2053350fd7c3a74217d406b71df1e0b0b
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2022-04-23 07:47:29 +0100

    learn about the immutability and mutability of string literals and the String type

--- projects/ownerships/src/main.rs
+++ projects/ownerships/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    let _s = "hello";
+    // let _s = "hello"; // immutable
+    let mut s = String::from("hello"); // mutable
+
+    s.push_str(", world!");
+
+    println!("{}", s);
 }
