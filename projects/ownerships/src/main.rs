931254a81046d88cb245b2d5039347a0679e6459
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2022-04-23 10:09:27 +0100

    learn about how move and copy works for functions

--- projects/ownerships/src/main.rs
+++ projects/ownerships/src/main.rs
@@ -6,6 +6,9 @@ fn main() {
 
     println!("{}", s);
 
+
+
+
     // Ways variables and data interact. : move
 
     let _x = 5; // binds 5 to variable x
@@ -18,11 +21,16 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1; // this a move rather than a copy, because the first variable is invalidated in order to avoid double free error.
 
-    // Rust copied the pointers to the data on the heap for s1
-    // and binds it to s2, but it doesn't make a copy of the data in heap
-    // itself, because that would be too expensive if the data on the heap were large.
+    /*
+    Rust copied the pointers to the data on the heap for s1
+    and binds it to s2, but it doesn't make a copy of the data in heap
+    itself, because that would be too expensive if the data on the heap were large.
+    */
 
     println!("{}, world!", s2);
+
+
+
 
     // Ways variables and data interact: Clone
 
@@ -30,4 +38,30 @@ fn main() {
     let s4 = s3.clone(); // this is a deep-copy; the heap data and pointers in stack is copy and binded to this new variable.
 
     println!("s3 = {}, s4 = {}", s3, s4);
+
+
+    
+
+
+    // Ownership and Functions.
+    let a = String::from("hello");
+
+    takes_ownership(a);
+
+    // println!("{}", a); // throws a compile time error because the varaible has been invalidated by rust
+
+    let z = 5;
+
+    makes_copy(z);
+
+    println!("{}", z);
+
 }
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // some_string goes out of scope and the 'drop' is called. The backing memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // some-integer goes out of scope. Nothing special happens.
