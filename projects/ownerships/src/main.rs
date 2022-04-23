887ef8fdfeff4168bf49d2db7be066ea5b66eef6
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2022-04-23 09:17:11 +0100

    learn about how String type variable is invalidated when reassigned to another variable in order to avoid the double free error

--- projects/ownerships/src/main.rs
+++ projects/ownerships/src/main.rs
@@ -5,4 +5,21 @@ fn main() {
     s.push_str(", world!");
 
     println!("{}", s);
+
+    // Ways variables and data interact.
+    let _x = 5; // binds 5 to variable x
+    let _y = _x; // make a copy of x and bind to y
+
+    // Both variable x and y are of fixed size, hence get pushed onto the stack.
+
+    // The case is different when it comes to the String type.
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    // Rust copied the pointers to the data on the heap for s1
+    // and binds it to s2, but it doesn't make a copy of the data in heap
+    // itself, because that would be too expensive if the data on the heap were large.
+
+    println!("{}, world!", s2);
 }
