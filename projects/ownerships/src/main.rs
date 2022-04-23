1e2ec2df7a3d9ae58574e15e3ef59b573e54d1a4
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2022-04-23 09:23:17 +0100

    learn about deep copying of heap data by using the clone method

--- projects/ownerships/src/main.rs
+++ projects/ownerships/src/main.rs
@@ -6,7 +6,8 @@ fn main() {
 
     println!("{}", s);
 
-    // Ways variables and data interact.
+    // Ways variables and data interact. : move
+
     let _x = 5; // binds 5 to variable x
     let _y = _x; // make a copy of x and bind to y
 
@@ -15,11 +16,18 @@ fn main() {
     // The case is different when it comes to the String type.
 
     let s1 = String::from("hello");
-    let s2 = s1;
+    let s2 = s1; // this a move rather than a copy, because the first variable is invalidated in order to avoid double free error.
 
     // Rust copied the pointers to the data on the heap for s1
     // and binds it to s2, but it doesn't make a copy of the data in heap
     // itself, because that would be too expensive if the data on the heap were large.
 
     println!("{}, world!", s2);
+
+    // Ways variables and data interact: Clone
+
+    let s3 = String::from("hello s3");
+    let s4 = s3.clone(); // this is a deep-copy; the heap data and pointers in stack is copy and binded to this new variable.
+
+    println!("s3 = {}, s4 = {}", s3, s4);
 }
